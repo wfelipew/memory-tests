ff92a611e9e1da968285d5129fdf0cd432993112
William Felipe Welter <wfelipew@gmail.com>
2020-10-27 00:57:42 -0300

    Add hugetlbpage examples

---
+++ hugepage-off.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+int main(int argc, char* argv[]){
+	char  *ptr, *tmp_ptr;
+
+
+	unsigned long i;
+	if(argc < 2){
+		printf("Usage: <Size allocation (megabytes)>");
+	}
+	int megabytes = atoi(argv[1]);
+        ptr=mmap(0,1024*1024* (unsigned long) megabytes,PROT_WRITE|PROT_READ,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+
+	if(ptr==MAP_FAILED){
+                printf("Falha ao alocar esta quantidade de memoria\n");
+                return 1;
+        }
+	tmp_ptr=ptr;
+	for(i=0;i+1<1024*1024* (unsigned long) megabytes; i++){
+                strcpy(tmp_ptr,"a");
+		tmp_ptr++;
+	}
+
+	printf("Memoria alocada\n");
+	munmap(ptr,1024*1024* (unsigned long) megabytes);
+	printf("Memoria desalocada\n");
+	return 0;
+}
+
+
---
+++ oom-killer.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+	char  *ptr, *tmp_ptr;
+	unsigned long i;
+	if(argc < 2){
+		printf("Usage: <Size allocation (megabytes)>");
+	}
+	int megabytes = atoi(argv[1]);
+
+        printf("Pressione qualquer tecla para alocar a memoria:\n");
+        getchar();
+
+	ptr=malloc(1024*1024* (unsigned long) megabytes);
+
+	if(ptr==NULL){
+                printf("Falha ao alocar esta quantidade de memoria\n");
+                return 1;
+        }
+
+	printf("Pressione qualquer tecla para inicializar a memoria:\n");
+        getchar();
+
+	tmp_ptr=ptr;
+	for(i=0;i<1024*1024* (unsigned long) megabytes; i++){
+                strcpy(tmp_ptr,"a");
+		tmp_ptr++;
+	}
+
+	printf("Memoria alocada, pressiona qualquer tecla para desalocar\n");
+	getchar();
+	free(ptr);
+	printf("Memoria desalocada, pressiona qualquer tecla para encerrar\n");
+	getchar();
+	return 0;
+}
+
+
